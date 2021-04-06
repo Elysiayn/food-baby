118ed47ea65f57e79e18dc8d236c9ead650699fb
Mila Decker <milacdecker@gmail.com>
2021-04-06 00:22:21 -0400

    fixed indexedDB helper function

--- client/src/utils/helpers.js
+++ client/src/utils/helpers.js
@@ -11,7 +11,7 @@ export function idbPromise(storeName, method, object) {
             const db = request.result;
 
             db.createObjectStore('menuItems', { keyPath: '_id' });
-            db.createObjectStore('categories' { keyPath: '_id' });
+            db.createObjectStore('categories', { keyPath: '_id' });
             db.createObjectStore('cart', { keyPath: '_id' });
         };
 
