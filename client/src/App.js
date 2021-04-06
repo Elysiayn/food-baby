0d3f1b910fa10cb2147bdf3344a52cfeabeb95ec
Elysiayn <wendy.lemus.wl@gmail.com>
2021-04-05 22:16:02 -0400

    enabled Nav in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -8,7 +8,7 @@ import Home from "./pages/Home";
 import NoMatch from "./pages/NoMatch"; 
 // import Login from "./pages/Login"; 
 // import Signup from "./pages/Signup"; 
-// import Nav from "./components/Nav"; 
+import Nav from "./components/Nav"; 
 import { StoreProvider } from "./utils/GlobalState"; 
 // import Success from "./pages/Success"; 
 // import OrderHistory from "./pages/OrderHistory"; 
@@ -31,7 +31,7 @@ function App() {
       <Router>
         <div>
           <StoreProvider> 
-            {/* <Nav />  */}
+            <Nav /> 
             <Switch>
               <Route exact path="/" component={Home} />
               {/* <Route exact path="/login" component={Login} />  */}
