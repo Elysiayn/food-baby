028f48f3298addfb485aa28bdbb5b2ec33782384
Elysiayn <wendy.lemus.wl@gmail.com>
2021-04-05 22:07:17 -0400

    added nav component folder and index.js

---
+++ client/src/components/Nav/index.js
@@ -0,0 +1,58 @@
+import React from "react";
+
+import { Link } from "react-router-dom";
+
+import Auth from "../../utils/auth";
+
+function Nav() {
+
+  function showNavigation() {
+    if (Auth.loggedIn()) {
+      return (
+        <ul className="flex-row">
+          <li className="mx-1">
+            <Link to="/orderHistory">
+              OrderHistory
+            </Link>
+          </li>
+          <li className="mx-1">
+            <a href="/" onClick={() => Auth.logout()}>
+              Logout
+            </a>
+          </li>
+        </ul>
+      );
+    } else {
+      return (
+        <ul className="flex-row">
+          <li className="mx-1">
+            <Link to="/signup">
+              Signup
+            </Link>
+          </li>
+          <li className="mx-1">
+            <Link className="mx-1">
+              Login
+            </Link>
+          </li>
+        </ul>
+      );
+    }
+  }
+
+  return (
+    <header className="flex-row px-1">
+      <h1>
+        <Link to="/">
+          <span role="img" aria-label="shopping bag">🛍️</span>
+        </Link>
+      </h1>
+
+      <nav>
+        {showNavigation()}
+      </nav>
+    </header>
+  );
+}
+
+export default Nav;
