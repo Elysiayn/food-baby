c12d53462ee18beea4a9c8fc75c7bf3dc97dfe94
Clu <retsulcconnor@gmail.com>
2021-04-12 18:32:10 -0400

    Added more dark themes to project. Almost have every element targeted properly. Working on making borders discernable during dark mode

--- client/src/components/Themes/GlobalStyles.js
+++ client/src/components/Themes/GlobalStyles.js
@@ -8,7 +8,8 @@ export const GlobalStyles = createGlobalStyle`
   & .ui.segment.menu {
     background: ${({ theme }) => theme.body};
     color: ${({ theme }) => theme.text};
-    transition: all 0.50s linear;
+    box-shadow: 2px -2px
+   
   }
   & .ui.large.header.menu-title {
     background: ${({ theme }) => theme.body};
@@ -19,15 +20,40 @@ export const GlobalStyles = createGlobalStyle`
     color: ${({ theme }) => theme.text};
   }
 
-  & .ui.card {
+  & .ui.card>.content {
     background: ${({ theme }) => theme.body};
     color: ${({ theme }) => theme.text};
   }
 
 
-  & .ui.sticky {
+  & div.ui.segment.cart {
     background: ${({ theme }) => theme.body};
     color: ${({ theme }) => theme.text};
-    transition: all 0.50s linear;
+    box-shadow
   }
+
+  & div.cart.body {
+    background: ${({ theme }) => theme.body};
+    color: ${({ theme }) => theme.text};
+  }
+
+  & ol.ui.list li:before {
+    background: ${({ theme }) => theme.body};
+    color: ${({ theme }) => theme.text};
+  }
+
+  & div.ui.medium.dividing.header {
+    background: ${({ theme }) => theme.body};
+    color: ${({ theme }) => theme.text};
+  }
+
+  & div.ui.small.header {
+    background: ${({ theme }) => theme.body};
+    color: ${({ theme }) => theme.text};
+  }
+
+  & li::before {
+    background: ${({ theme }) => theme.body};
+    color: ${({ theme }) => theme.text};
+}
   `
